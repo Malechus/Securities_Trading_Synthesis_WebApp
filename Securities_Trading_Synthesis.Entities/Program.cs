30ee27e19721d0a213fad1c22a00f0f47cbece82
Garrett Kelly <grkelly@lb.com>
2021-04-18 16:09:37 -0400

    Adds Entities Namespace
    Scaffolding and project building.

---
+++ Securities_Trading_Synthesis.Entities/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Configuration;
+
+namespace Securities_Trading_Synthesis.Entities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
