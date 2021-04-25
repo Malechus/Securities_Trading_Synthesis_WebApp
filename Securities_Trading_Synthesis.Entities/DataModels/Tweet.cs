1b8d625bbe118dfc9484f0a43b5d34e53900ed7b
Garrett Kelly <grkelly@lb.com>
2021-04-25 19:00:59 -0400

    Creates Enitties prj, tweets class, dbcontext.
    Entities will exapnd to include all types for this solution.

---
+++ Securities_Trading_Synthesis.Entities/DataModels/Tweet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Securities_Trading_Synthesis.Entities.DataModels
+{
+    public class Tweet
+    {
+        public int ID { get; set; }
+        public DateTime Post_Date { get; set; }
+        public string Keywords { get; set; }
+        public string User_Name { get; set; }
+        public string Tags { get; set; }
+        public string Security_Sym { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ Securities_Trading_Synthesis.Entities/STSContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+using Securities_Trading_Synthesis.Entities.DataModels;
+
+namespace Securities_Trading_Synthesis.Entities
+{
+    public class STSContext : DbContext
+    {
+        public STSContext()
+            : base("name=STSConnection")
+        {
+
+        }
+
+        public DbSet<Tweet> Tweets { get; set; }
+    }
+}
